efe2d2ae459e4f8bdb2e4120bdacf1dfe4fd08f3
huangzhiqiang <w739932799@qq.com>
2020-03-03 14:30:35 +0800

    MaterialApp：使用界面组件与定制界面主题

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,28 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      //主题
+      theme: ThemeData(primarySwatch: Colors.yellow),
+
+      //主题颜色字体
+      home: Scaffold(
+        appBar: AppBar(
+            elevation: 20,
+            title: Text("小强",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 40, color: Colors.green))),
+        //主题下面的内容
+        body: Hello(),
+      ),
+    );
+  }
+}
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return Center(
       child: Text(
         "小强",
