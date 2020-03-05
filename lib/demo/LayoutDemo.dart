0005451774e828e68aefbc59d8459527f3728c35
huangzhiqiang <w739932799@qq.com>
2020-03-05 08:53:01 +0800

    IconBadge小部件

--- lib/demo/LayoutDemo.dart
+++ lib/demo/LayoutDemo.dart
@@ -12,6 +12,35 @@ import 'package:flutter/material.dart';
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        IconBadge(
+          Icons.clear,
+        )
+      ],
+    );
+  }
+}
+
+class IconBadge extends StatelessWidget {
+  final double mSize;
+
+  final IconData mIconData;
+
+  //构造器
+  IconBadge(this.mIconData, {this.mSize = 30.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(
+        mIconData,
+        size: mSize,
+        color: Colors.grey,
+      ),
+      height: mSize + 60,
+      width: mSize + 60,
+      color: Colors.blue,
+    );
   }
 }
