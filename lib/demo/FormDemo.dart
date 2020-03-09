aa56638ed5a3d41fb79e762714e5a928162e38ce
huangzhiqiang <w739932799@qq.com>
2020-03-09 09:32:58 +0800

    TextField：文本字段

--- lib/demo/FormDemo.dart
+++ lib/demo/FormDemo.dart
@@ -15,14 +15,32 @@ class FormDemo extends StatelessWidget {
     return Scaffold(
       body: Theme(
         data: Theme.of(context).copyWith(
-          primaryColor: Colors.yellow,
+          primaryColor: Colors.orange,
         ),
-        child: ThemeDemo(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[ThemeDemo(), TextFieldDemo()],
+
+        ),
       ),
     );
   }
 }
 
+class TextFieldDemo extends StatefulWidget {
+  @override
+  _TextFieldDemoState createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
+
+///主题
 class ThemeDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
