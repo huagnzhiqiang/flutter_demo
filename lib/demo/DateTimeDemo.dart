0ced73327ae9938a86cb635db4d476ed7de5e4ad
huangzhiqiang <w739932799@qq.com>
2020-03-26 15:45:47 +0800

    显示格式化日期

--- lib/demo/DateTimeDemo.dart
+++ lib/demo/DateTimeDemo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 /*
  * @author 小强
  *
@@ -14,15 +15,31 @@ class DateTimeDemo extends StatefulWidget {
 }
 
 class _DateTimeDemoState extends State<DateTimeDemo> {
+  final DateTime dt = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("时间选择器"),),
-
+      appBar: AppBar(
+        title: Text("时间选择器"),
+      ),
       body: Container(
+        padding: EdgeInsets.all(10),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
+            InkWell(
+              onTap: () {},
+              child: Row(
+                children: <Widget>[
+                  Text(DateFormat.yMd().format(dt)),
+                  Icon(Icons.arrow_drop_down),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
