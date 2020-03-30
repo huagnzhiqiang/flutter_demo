6a26a673f441b0f14bdfef4ff6e02d448b4c22f9
huangzhiqiang <w739932799@qq.com>
2020-03-30 11:15:07 +0800

    Chip：用列表生成带删除功能的小碎片

--- lib/demo/ChipDemo.dart
+++ lib/demo/ChipDemo.dart
@@ -15,61 +15,92 @@ class ChipDemo extends StatefulWidget {
 }
 
 class _ChipDemoState extends State<ChipDemo> {
+  List<String> list = ["Apple", "Banana", "Lemon"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("小碎片"),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Wrap(
-              spacing: 10.0,
-              runSpacing: 10.0,
-              children: <Widget>[
-                Chip(label: Text("小强")),
-                Chip(
-                  label: Text("小强"),
-                  backgroundColor: Colors.orange,
-                ),
-                Chip(
-                  label: Text("小强"),
-                  avatar: CircleAvatar(
-                    backgroundColor: Colors.orange,
-                    child: Text("强"),
-                  ),
-                ),
-                Chip(
-                  label: Text("小强"),
-                  avatar: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "https://resources.ninghao.org/images/overkill.png"),
-                  ),
-                ),
-                Divider(
-                  color: Colors.orange,
-                  height: 20.0,
-                  indent: 15.0,
-                ),
-                Chip(
-                  label: Text("删除演示"),
-                  onDeleted: () {},
-                  deleteIcon: Icon(Icons.delete),
-                  deleteIconColor: Colors.red,
-                  deleteButtonTooltipMessage: "长按删除",
-                  avatar: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "https://resources.ninghao.org/images/overkill.png"),
-                  ),
-                ),
-              ],
-            )
-          ],
+        appBar: AppBar(
+          title: Text("小碎片"),
         ),
-      ),
-    );
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Wrap(
+                spacing: 10.0,
+                runSpacing: 10.0,
+                children: <Widget>[
+                  Chip(label: Text("小强")),
+                  Chip(
+                    label: Text("小强"),
+                    backgroundColor: Colors.orange,
+                  ),
+                  Chip(
+                    label: Text("小强"),
+                    avatar: CircleAvatar(
+                      backgroundColor: Colors.orange,
+                      child: Text("强"),
+                    ),
+                  ),
+                  Chip(
+                    label: Text("小强"),
+                    avatar: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://resources.ninghao.org/images/overkill.png"),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.orange,
+                    height: 20.0,
+                    indent: 15.0,
+                  ),
+                  Chip(
+                    label: Text("删除演示"),
+                    onDeleted: () {},
+                    deleteIcon: Icon(Icons.delete),
+                    deleteIconColor: Colors.red,
+                    deleteButtonTooltipMessage: "长按删除",
+                    avatar: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://resources.ninghao.org/images/overkill.png"),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.orange,
+                    height: 20.0,
+                    indent: 15.0,
+                  ),
+                  Wrap(
+                    children: list.map((String data) {
+                      return Chip(
+                        label: Text(data),
+                        onDeleted: () {
+                          setState(() {
+                            list.remove(data);
+                          });
+                        },
+                        deleteIcon: Icon(Icons.delete),
+                        deleteIconColor: Colors.red,
+                        deleteButtonTooltipMessage: "长按删除",
+                        avatar: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "https://resources.ninghao.org/images/overkill.png"),
+                        ),
+                      );
+                    }).toList(),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                list = ["Apple", "Banana", "Lemon"];
+              });
+            },
+            child: Icon(Icons.restore)));
   }
 }
