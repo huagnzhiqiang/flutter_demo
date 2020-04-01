eff4e09008f0e0da1aedd9169c608301f89f304e
huangzhiqiang <w739932799@qq.com>
2020-04-01 16:12:52 +0800

    从父辈那里传递个回调

--- lib/demo/state/StateManagementDemo.dart
+++ lib/demo/state/StateManagementDemo.dart
@@ -18,31 +18,35 @@ class StateManagementDemo extends StatefulWidget {
 class _StateManagementDemo extends State<StateManagementDemo> {
     var _count = 0;
 
+    void setOnPressed() {
+        setState(() {
+            _count += 1;
+        });
+    }
+
     @override
     Widget build(BuildContext context) {
         return Scaffold(
             appBar: AppBar(title: Text("状态管理"),),
-            body: Counter(_count),
-            floatingActionButton: FloatingActionButton(onPressed: () {
-                setState(() {
-                    _count += 1;
-                });
-            }, child: Icon(Icons.add),),
+            body: Counter(_count, setOnPressed),
+            floatingActionButton: FloatingActionButton(
+                onPressed: setOnPressed, child: Icon(Icons.add),),
         );
     }
 }
 
+
 class Counter extends StatelessWidget {
-  final int count ;
+    final int count;
 
+    final VoidCallback setOnPressed;
 
-  Counter(this.count);
+    Counter(this.count, this.setOnPressed);
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-        child: Chip(label: Text("$count")),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return Center(
+            child: ActionChip(label: Text("$count"), onPressed: setOnPressed,),
+        );
+    }
 }
